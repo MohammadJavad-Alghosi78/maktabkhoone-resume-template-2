6d1cb6b18327c765a4509dd23a0baa479f5b87aa
Milad <javad.alghosi7904@gmail.com>
2022-12-20 19:23:43 +0330

    create language page

--- website/views.py
+++ website/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def language_view(request):
-    return render(request, 'website/language.html')
+    context = {
+        "first_language": {
+            'value': 'Persian',
+            'rate': '100%'
+        },
+        "second_language": {
+            'value': 'English',
+            'rate': '50%'
+        },
+        'third_language': {
+            'value': 'Arabic',
+            'rate': '20%'
+        }
+    }
+    return render(request, 'website/language.html', context)
 
 def education_view(request):
     pass
